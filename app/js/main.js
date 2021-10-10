ab028df0998da878eeee3e0cef1d9b9e9f10c525
Ihar Zapolskj <alonemoutain@gmail.com>
2021-10-10 10:05:41 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.header__btn').on('click', function() {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function() {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+
+    $('.top__slider').slick({})
+})
